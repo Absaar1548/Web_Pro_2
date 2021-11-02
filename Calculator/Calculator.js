db8897be93995fb2f24f0e42eb1d40f66cc1f7e2
Absaar-Ali <absaarali2256@gmail.com>
2021-11-02 15:01:23 +0530

    First Commit

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -3,7 +3,18 @@ function calculate()
     event.preventDefault();
     var a = parseFloat(document.getElementById("a").value);
     var b = parseFloat(document.getElementById("b").value);
-    document.getElementById("answer").value=a+b;
+    var c = document.getElementById("c").value;
+    if(c=="+")
+        document.getElementById("answer").value=a+b;
+    else if(c=="-")
+        document.getElementById("answer").value=a-b;
+    else if(c=="*")
+        document.getElementById("answer").value=a*b;
+    else if(c=="/")
+        document.getElementById("answer").value=a/b;
+    else if(c=="^")
+        document.getElementById("answer").value=Math.pow(a,b);
+
     document.getElementById("ans").style.display="block";
 
 }
